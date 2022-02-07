d4e0950cfe7e96ffc243ea2c561ff138ec396f56
Jose Rosendo <josemrb99@gmail.com>
2022-02-07 19:12:02 +0100

    User service API calls were implemented

---
+++ src/services/EmployeeService/index.ts
@@ -0,0 +1,81 @@
+const API_URL = 'http://dummy.restapiexample.com/api/v1';
+
+export const getAllEmployees = async () => {
+    try {
+        const res = await (await fetch(`${API_URL}/employees`, {
+            method: 'GET',
+        })).json();
+
+        return res;
+    } catch (err) {
+        console.log(err);
+
+        return undefined;
+    }
+};
+
+export const getSingleEmployee = async (id: number) => {
+    try {
+        const res = await (await fetch(`${API_URL}/employee/${id}`, {
+            method: 'GET',
+        })).json();
+
+        return res;
+    } catch (err) {
+        console.log(err);
+
+        return undefined;
+    }
+};
+
+export const createEmployee = async (name: string, salary: number, age: number) => {
+    try {
+        const res = await (await fetch(`${API_URL}/create`, {
+            method: 'POST',
+            body: JSON.stringify({
+                name,
+                salary,
+                age,
+            }),
+        })).json();
+
+        return res;
+    } catch (err) {
+        console.log(err);
+
+        return undefined;
+    }
+};
+
+export const updateEmployee = async (id: number, name?: string, salary?: number, age?: number) => {
+    try {
+        const res = await (await fetch(`${API_URL}/update/${id}`, {
+            method: 'PUT',
+            body: JSON.stringify({
+                name,
+                salary,
+                age,
+            }),
+        })).json();
+
+        return res;
+    } catch (err) {
+        console.log(err);
+
+        return undefined;
+    }
+};
+
+export const deleteEmployee = async (id: number) => {
+    try {
+        const res = await (await fetch(`${API_URL}/delete/${id}`, {
+            method: 'DELETE',
+        })).json();
+
+        return res;
+    } catch (err) {
+        console.log(err);
+
+        return undefined;
+    }
+};
