12e00dc5eb40d6207a89a5df187e82f386b1a11f
Jose Rosendo <josemrb99@gmail.com>
2022-02-07 19:25:39 +0100

    Intall React Router

---
+++ src/components/pages/employeeDetail/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text } from '@chakra-ui/react';
+
+const EmployeeDetail = () => {
+    return (
+        <Text>
+            Employee Detail
+        </Text>
+    );
+}
+
+export default EmployeeDetail;
