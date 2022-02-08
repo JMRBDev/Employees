ace55d630acbbafbbd90a9232ad8abcbac771b06
Jose Rosendo <josemrb99@gmail.com>
2022-02-08 21:37:24 +0100

    Redux persistence implemented

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import { store } from './redux/store';
+import { store, persistor } from './redux/store';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,15 +1,44 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+    PersistConfig,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import appReducer from './slices/appSlice';
 import userPreferencesReducer from './slices/userPreferencesSlice';
 import employeesReducer from './slices/employeesSlice';
 
-export const store = configureStore({
-    reducer: {
-        app: appReducer,
-        userPreferences: userPreferencesReducer,
-        employees: employeesReducer,
-    },
+const persistConfig: PersistConfig<any> = {
+    key: 'root',
+    version: 1,
+    storage,
+};
+
+const rootReducer = combineReducers({
+    app: appReducer,
+    userPreferences: userPreferencesReducer,
+    employees: employeesReducer,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        }
+    }),
+});
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
