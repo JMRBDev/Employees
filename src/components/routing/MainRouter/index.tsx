a0939c9cdf345bb00a9844cabea44cc61fde5745
Jose Rosendo <josemrb99@gmail.com>
2022-02-08 15:20:14 +0100

    Added employee creation and details routes

--- src/components/routing/MainRouter/index.tsx
+++ src/components/routing/MainRouter/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from '../../pages/homePage';
 import EmployeePage from '../../pages/employeePage';
+import { Text } from '@chakra-ui/react';
 
 const MainRouter = () => {
     return (
         <Routes>
             <Route path="/" element={<HomePage />} />
-            <Route path="employee" element={<EmployeePage />} />
+            <Route path="new" element={<Text>Employee creation form</Text>} />
+            <Route path="employee/:id" element={<EmployeePage />} />
         </Routes>
     );
 }
